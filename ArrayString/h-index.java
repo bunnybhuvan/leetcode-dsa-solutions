2a5a359808498bfbd2f7817ad707b737bffb6f35
Bunny <99382220+bunnybhuvan@users.noreply.github.com>
2026-03-21 18:29:30 +0530

    Add files via upload

---
+++ ArrayString/h-index.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        // Step 1: Sort చేసి reverse చేయి
+        Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+        
+        // Step 2: Reversed traverse (largest first)
+        for (int i = 0; i < n; i++) {
+            // n-i = right నుండి left కి వెళ్తున్నాం
+            if (citations[n - 1 - i] >= i + 1) {
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+}
